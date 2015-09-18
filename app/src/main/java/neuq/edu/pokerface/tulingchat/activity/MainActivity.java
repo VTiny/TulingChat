7125181383e6c0a0e8c62729e32e09e1a7837e07
pokerfacelx <pokerface_lx@foxmail.com>
2015-09-18 14:23:16 +0800

    basic info and receive

--- app/src/main/java/neuq/edu/pokerface/tulingchat/activity/MainActivity.java
+++ app/src/main/java/neuq/edu/pokerface/tulingchat/activity/MainActivity.java
@@ -3,6 +3,10 @@ package neuq.edu.pokerface.tulingchat.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import neuq.edu.pokerface.tulingchat.R;
 import neuq.edu.pokerface.tulingchat.model.Data;
@@ -13,25 +17,48 @@ import retrofit.client.Response;
 
 public class MainActivity extends Activity {
 
-    static final String KEY = "de5ad10269d30982903b48df4aaec722";
+    static final String KEY = "823098b74cf1c8d9dc62c55f79441df5";
+    private String info;
+    private EditText editText;
+    private Button button;
+    private TextView txtInfo;
+    private TextView txtReceived;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String info = "你是谁";
-
-        ChatUtil.service.getData(KEY, info, new Callback<Data>() {
+        button = (Button) findViewById(R.id.btn_input);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void success(Data data, Response response) {
-                Log.e("---------code-------", String.valueOf(data.getCode()));
-                Log.e("---------text-------", data.getText());
-            }
+            public void onClick(View v) {
+                editText = (EditText) findViewById(R.id.edit_input);
+                info = editText.getText().toString();
 
-            @Override
-            public void failure(RetrofitError error) {
+                txtInfo = (TextView) findViewById(R.id.txt_input);
+                txtReceived = (TextView) findViewById(R.id.txt_receive);
 
+                if (!info.isEmpty()) {
+                    ChatUtil.service.getData(KEY, info, new Callback<Data>() {
+                        @Override
+                        public void success(Data data, Response response) {
+                            Log.e("------code-----", String.valueOf(data.getCode()));
+                            //Log.e("------text-----", data.getText());
+
+                            txtInfo.setText(info);
+                            txtInfo.setVisibility(View.VISIBLE);
+                            txtReceived.setText(data.getText());
+                            txtReceived.setVisibility(View.VISIBLE);
+
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+
+                        }
+                    });
+                }
             }
         });
 
